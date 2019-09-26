5ce52468761950a39acc385449d029acbe5f8a8f
Adam Brooks <52460829+atbrooks84@users.noreply.github.com>
2019-09-26 16:51:48 -0400

    Add project files.

---
+++ WeatherApp/Weather.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp
+{
+    class Weather
+    {
+        public string Title { get; set; }
+        public string Temperature { get; set; }
+        public string Wind { get; set; }
+        public string Humidity { get; set; }
+        public string Visibility { get; set; }
+        public string Sunrise { get; set; }
+        public string Sunset { get; set; }
+
+        public Weather()
+        {
+            Title = "";
+            Temperature = "";
+            Wind = "";
+            Humidity = "";
+            Sunrise = "";
+            Sunset = "";
+        }
+    }
+}
---
+++ WeatherApp/WeatherPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WeatherApp
+{
+        public partial class WeatherPage : ContentPage
+    {
+        public WeatherPage()
+        {
+            InitializeComponent();
+            this.Title = "Sample Weather App";
+            getWeatherBtn.Clicked += GetWeatherBtn_Clicked;
+
+            this.BindingContext = new Weather();
+        }
+
+        private async void GetWeatherBtn_Clicked(object sender, EventArgs e)
+        {
+            Weather weather = await Core.GetWeather("27203");
+            getWeatherBtn.Text = weather.Title;
+        }
+    }
+}
